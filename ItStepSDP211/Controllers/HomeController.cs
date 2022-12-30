8c6e7b0edce385aaea97b13185e724a7ced712ce
77001803540 <zhasulanasainov@gmail.com>
2022-12-30 20:09:07 +0500

    Last updates 30.12.2022

--- ItStepSDP211/Controllers/HomeController.cs
+++ ItStepSDP211/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ItStepSDP211.Models;
+using Microsoft.Ajax.Utilities;
+using ItStepSDP211.Util;
 
 namespace ItStepSDP211.Controllers
 {
@@ -16,8 +18,47 @@ namespace ItStepSDP211.Controllers
             ViewBag.Movies = movies;
 
             return View();
-
         }
-      
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.MovieId = id;
+
+            return View();
+        }
+        [HttpPost]
+        public string Buy(BuyTickets buyTickets)
+        {
+            buyTickets.Date = DateTime.Now;
+            buyTickets.Price = 800;
+            buyTickets.Email = "qwerty@qwerty";
+            buyTickets.Person = "Jhon";
+
+            db.Tickets.Add(buyTickets);
+
+            db.SaveChanges();
+
+            return "Thank you for purchasing our ticket " + buyTickets.Person +" ! ";
+        }
+
+        private DateTime detToday()
+        {
+            return DateTime.Now;
+        }
+
+
+        public string Square()
+        {
+            int a = Int32.Parse(Request.Params["a"]);
+            int h = Int32.Parse(Request.Params["h"]);
+            double s = a * h / 2.0;
+            return "<h2> a = " + a + "h = "+ h +" Square =" + s +"</h2>" ;
+        }
+
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("Hello world !");
+        }
     }
 }
